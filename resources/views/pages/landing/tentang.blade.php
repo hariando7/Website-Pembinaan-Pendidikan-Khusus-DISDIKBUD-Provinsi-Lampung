6d3fad82a0ed32045d077c56ade9f514814d848b
hariando7 <rianaja2200022@gmail.com>
2024-02-20 17:01:15 +0700

    update-landing

--- resources/views/pages/landing/tentang.blade.php
+++ resources/views/pages/landing/tentang.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tentang</title>
+    <title>Tentang | Bidang Pembinaan Pendidikan Khusus | Disdikbud Provinsi Lampung</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
-
+    <link rel="icon" type="image/x-icon" href="assets/landing/prov-lampung2.svg">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@2.6.0/dist/full.css" rel="stylesheet" type="text/css" />
 
     <!-- Slick Carousel CSS -->
